2b1a6b397ba892bb4b5a1c6ce973a196bb24cce4
viniciuspch <vinicius.geral2018@gmail.com>
2024-03-19 11:09:49 -0300

    Adicionado primeiros arquivos para inicio do projeto

---
+++ src/main/java/com/poc/diospringboot/DioSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.poc.diospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/diospringboot/DioSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.diospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
